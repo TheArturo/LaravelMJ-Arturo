d85636d88d424efeeae8cd9c9c5627c6494e4364
Cesar <arturo153cesaryt@gmail.com>
2025-08-24 09:58:22 -0500

    correcion seeders

---
+++ src/Producto/Models/Producto.php
@@ -0,0 +1,14 @@
+<?php
+namespace Src\Producto\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio',
+        'stock',
+    ];
+}
